04af7c0d9f38657ecab7a63e97f2314ed8a99a96
Mantas <mantas.bertasius@gmail.com>
2023-04-04 22:16:56 +0300

    added Sign in activity

---
+++ app/src/main/java/com/example/checkandgo2023/SignInActivity.kt
@@ -0,0 +1,11 @@
+package com.example.checkandgo2023
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
